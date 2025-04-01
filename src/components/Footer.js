f79882e9ea798e2fdab15eec8c5ef454c6886503
Gift Amadi <amadigiftchukwudi@gmail.com>
2025-04-01 09:38:54 +0100

    second commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -18,7 +18,7 @@ const Footer = () => {
           const { id, href, icon } = link;
           return (
             <li>
-              <a href={href} target="_blank" className="footer-icon"
+              <a href={href} target="_blank" rel="noreferrer" className="footer-icon"
               ><i className={icon}></i></a>
             </li>
           )
